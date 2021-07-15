666093101e04079390bb2b5c9ccd6908717c5d4e
vv-young <45314906@qq.com>
2021-07-14 19:25:32 -0700

    1

--- v2/start.sh
+++ v2/start.sh
@@ -6,7 +6,7 @@ chmod +x cl xray
 
 /v2/xray -c /v2/v2.json &> /dev/null &
  
-/v2/cl tunnel --url http://localhost:8080 --no-chunked-encoding --origincert /v2/cert.pem --credentials-file /v2/8a599fc5-e520-4397-a6d5-47028c5ac399.json  run azus  &> /opt/1.log &
+/v2/cl tunnel --url http://localhost:8080 --no-chunked-encoding --origincert /v2/cert.pem --credentials-file /v2/6e8d50d3-d351-4908-a72d-c925de747bf2.json  run azhk  &> /opt/1.log &
 
 
 echo 'nameserver 8.8.8.8' > /etc/resolv.conf
